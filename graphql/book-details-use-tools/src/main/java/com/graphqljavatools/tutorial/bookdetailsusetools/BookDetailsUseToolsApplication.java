6e248343dad94dc30b495b68878624b0bdb49610
luoji <gunsluo@gmail.com>
2019-08-12 14:01:47 +0800

    graphql project
    Signed-off-by: luoji <gunsluo@gmail.com>

---
+++ graphql/book-details-use-tools/src/main/java/com/graphqljavatools/tutorial/bookdetailsusetools/BookDetailsUseToolsApplication.java
@@ -0,0 +1,13 @@
+package com.graphqljavatools.tutorial.bookdetailsusetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDetailsUseToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDetailsUseToolsApplication.class, args);
+	}
+
+}
